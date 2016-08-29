3971a23003d539f027bcd7780a0c2f9fa1ee6f3f
Matías Beretta <msberetta92@gmail.com>
2016-08-28 22:16:13 -0300

    Search web service

--- mobile2016c2_backend/routes/search.js
+++ mobile2016c2_backend/routes/search.js
@@ -10,12 +10,66 @@ router.get('/product', function(req, res, next) {
 router.get('/shop', function(req, res, next) {
 	helpers.processService(res, function(db){
 		
+		var filter = { exclusive: true };
+		if (req.query.criteria === "all") {
+			delete filter.exclusive;
+		}
+			
 		switch (req.query.query) {
 			
 			// TODO validate input params presence and format
 			
 			case "by_radius":
-				db.collection("shops").find({ location: { $nearSphere: { $geometry: { type: "Point", coordinates: [ Number(req.query.long), Number(req.query.lat) ] }, $maxDistance: 1000 * Number(req.query.radiusKm) } } }).toArray(function(err, docs){
+				filter.location = { $nearSphere: { $geometry: { type: "Point", coordinates: [ Number(req.query.long), Number(req.query.lat) ] }, $maxDistance: 1000 * Number(req.query.radiusKm) } };
+				db.collection("shops").find(filter).toArray(function(err, docs){
+					if (err) {
+						helpers.replyError(res);
+					} else {
+						res.json(docs);
+					}
+					helpers.finishService(db);
+				});
+				break;
+				
+			case "by_city":
+				filter.city = new mongo.ObjectID(req.query.id_city);
+				db.collection("shops").find(filter).toArray(function(err, docs){
+					if (err) {
+						helpers.replyError(res);
+					} else {
+						res.json(docs);
+					}
+					helpers.finishService(db);
+				});
+				break;
+				
+			case "by_country":
+				filter.country = new mongo.ObjectID(req.query.id_country);
+				db.collection("shops").find(filter).toArray(function(err, docs){
+					if (err) {
+						helpers.replyError(res);
+					} else {
+						res.json(docs);
+					}
+					helpers.finishService(db);
+				});
+				break;
+				
+			case "by_state":
+				filter.state = new mongo.ObjectID(req.query.id_state);
+				db.collection("shops").find(filter).toArray(function(err, docs){
+					if (err) {
+						helpers.replyError(res);
+					} else {
+						res.json(docs);
+					}
+					helpers.finishService(db);
+				});
+				break;
+				
+			case "by_continent":
+				filter.continent = new mongo.ObjectID(req.query.id_continent);
+				db.collection("shops").find(filter).toArray(function(err, docs){
 					if (err) {
 						helpers.replyError(res);
 					} else {
