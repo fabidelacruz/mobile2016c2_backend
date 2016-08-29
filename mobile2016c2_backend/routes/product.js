e193d47a33b13ff00b59bb7f87dbc8caf87c93c5
Matías Beretta <msberetta92@gmail.com>
2016-08-28 23:19:33 -0300

    Product & Shop web services

--- mobile2016c2_backend/routes/product.js
+++ mobile2016c2_backend/routes/product.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var helpers = require('./helpers');
+var mongo = require('mongodb');
 
 router.get('/shops', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	helpers.processService(res, function(db){
+		
+		// TODO validate input params presence and format
+		
+		db.collection("shops").find({ products: req.query.barcode }).toArray(function(err, docs){
+			if (err) {
+				helpers.replyError(res);
+			} else {
+				res.json(docs);
+			}
+			helpers.finishService(db);
+		});
+		
+	});
 });
 
 module.exports = router;
--- mobile2016c2_backend/routes/shop.js
+++ mobile2016c2_backend/routes/shop.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var helpers = require('./helpers');
+var mongo = require('mongodb');
 
 router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	helpers.processService(res, function(db){
+		
+		// TODO validate input params presence and format
+		
+		db.collection("shops").find({ _id: new mongo.ObjectID(req.query.id_shop) },{_id:0, products:1}).toArray(function(err, docs){
+			if (err) {
+				helpers.replyError(res);
+			} else {
+				res.json(helpers.emptyArrayOrProperty(docs, "products"));
+			}
+			helpers.finishService(db);
+		});
+		
+	});
 });
 
 module.exports = router;
