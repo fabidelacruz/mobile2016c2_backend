e24957194b8d6f1aa6246f40171a0e44054606b1
Matías Beretta <msberetta92@gmail.com>
2016-10-15 18:38:05 -0300

    Discounts

--- mobile2016c2_backend/routes/shop.js
+++ mobile2016c2_backend/routes/shop.js
@@ -20,4 +20,21 @@ router.get('/products', function(req, res, next) {
 	});
 });
 
+router.get('/discounts', function(req, res, next) {
+	helpers.processService(res, function(db){
+		
+		// TODO validate input params presence and format
+		
+		db.collection("shops").find({ _id: new mongo.ObjectID(req.query.id_shop) },{_id:0, discounts:1}).toArray(function(err, docs){
+			if (err) {
+				helpers.replyError(res);
+			} else {
+				res.json(helpers.emptyArrayOrProperty(docs, "discounts"));
+			}
+			helpers.finishService(db);
+		});
+		
+	});
+});
+
 module.exports = router;
